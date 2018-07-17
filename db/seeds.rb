2839fd4123f49b5ae599ed22271e1070aa36128e
ShinWoong Yoon <tlsdnd7897@likelion.org>
2018-07-17 09:46:34 +0000

    init

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+(1..3).each do |i|
+    User.create(username: i, email: "#{i}@#{i}.com", password: "123123", password_confirmation: "123123" )
+    (1..3).each do |j|
+        Post.create(title: "#{i}가 #{j}번 쨰 글을 작성했습니다.", content: "#{i}가 쓴 #{j}번 째 글입니다.", user_id:i)
+    end
+end
